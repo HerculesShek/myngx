23f2cb44610e7f424b124d8e5643b5faed5da684
Will Shek <xz.will.shek@gmail.com>
2018-12-24 16:08:34 +0800

    test echo

---
+++ auto/test/echo_capablity.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if echo "test\c" | grep c >/dev/null; then
+
+    if echo -n test | grep n >/dev/null; then
+        echo "1"
+        ngx_n=
+        ngx_c=
+
+    else
+        echo "2"
+        ngx_n=-n
+        ngx_c=
+    fi
+
+else # mac here
+    echo "3"
+    ngx_n=
+    ngx_c='\c'
+fi
