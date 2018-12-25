2b610eb3f41637aeb63e27d9b7319db54e1dee8b
Will Shek <xz.will.shek@gmail.com>
2018-12-25 12:13:34 +0800

    update

--- auto/test/regex_dirsep.sh
+++ auto/test/regex_dirsep.sh
@@ -3,5 +3,9 @@
 ngx_regex_dirsep="\/"
 ngx_dirsep='/'
 
+NGX_OBJS=objs
+ngx_objs_dir=${NGX_OBJS}${ngx_regex_dirsep}
+echo "ngx_objs_dir is $ngx_objs_dir"
+
 echo "ngx_regex_dirsep is '$ngx_regex_dirsep'"
 echo "ngx_dirsep is '$ngx_dirsep'"
